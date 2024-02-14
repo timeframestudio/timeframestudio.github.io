d1d6f282db03b58731ee887b127e0ff1ac698b89
Fuzzy Wuzzy <74162951+codeNinja-1@users.noreply.github.com>
2024-02-14 15:48:50 -0800

    Move code from mockup to scripts/home.js

--- dist/scripts/home.js
+++ dist/scripts/home.js
@@ -1,1 +1,9 @@
-console.log("hi!");
+document.addEventListener('scroll', () => {
+    const navBar = document.getElementById('navigationBar');
+
+    if (window.scrollY > 0) {
+        navBar.classList.add('navigation-bar-shadowed');
+    } else {
+        navBar.classList.remove('navigation-bar-shadowed');
+    }
+});
