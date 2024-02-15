8433dc6c6232b0dc99f8f7934a6fa17d52d22208
Imirami314 <98506195+Imirami314@users.noreply.github.com>
2024-02-14 17:58:22 -0800

    started displaying system for mapPoints
    it just draws squares right now but we will change that later
    also:
    - created projects.json file to store all the data for the different project, the code reads the file and displays a new map point

--- dist/scripts/home.js
+++ dist/scripts/home.js
@@ -1,1 +1,12 @@
+import './world-map.js';
 import './navigation.js';
+
+document.addEventListener('scroll', () => {
+    const navBar = document.getElementById('navigationBar');
+
+    if (window.scrollY > 0) {
+        navBar.classList.add('navigation-bar-shadowed');
+    } else {
+        navBar.classList.remove('navigation-bar-shadowed');
+    }
+});
