6397f678c16b06ea259fefdd367808bce41f70d7
Fuzzy Wuzzy <74162951+codeNinja-1@users.noreply.github.com>
2024-02-15 09:50:02 -0800

    Improve error message

--- dist/scripts/projects.js
+++ dist/scripts/projects.js
@@ -2,7 +2,7 @@ let projectSummaries = window['_projectSummaryData'];
 
 export async function getProjectSummaries() {
     if (!projectSummaries) {
-        throw new Error("Project summaries not injected properly");
+        throw new Error("Project summaries not injected properly. Is the server running?");
     }
 
     return projectSummaries;
