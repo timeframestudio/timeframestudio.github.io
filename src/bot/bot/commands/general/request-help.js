7a5fc7390db8e6c5622303b3f15d162661dd68d8
winterscode <qgv3981g@duck.com>
2024-03-07 09:46:48 -0800

    Converted commands to embeds + request-help

--- src/bot/bot/commands/general/request-help.js
+++ src/bot/bot/commands/general/request-help.js
@@ -1,5 +1,7 @@
 
-import { SlashCommandBuilder } from 'discord.js';
+import { SlashCommandBuilder, EmbedBuilder } from 'discord.js';
+
+import config from '../../../config.json' assert { type: 'json' };
 
 export const data = new SlashCommandBuilder()
     .setName('request-help')
@@ -18,8 +20,27 @@ export const data = new SlashCommandBuilder()
     .addStringOption(o => o
         .setName('summary')
         .setDescription('A short, 1-sentence summary of your problem')
-        .setRequired(false));
+        .setRequired(true));
 
 export async function execute(interaction) {
-    await interaction.reply('Pong!');
+
+    let departmentPing = `<@&${config.roles[interaction.options.getString('department')]}>`;
+    
+    const embed = new EmbedBuilder()
+        .setColor(0xddddff)
+        .setTitle('Help Request')
+        .setDescription(`Help has been requested for ${departmentPing} by <@${interaction.user.id}>.\n\n> "${interaction.options.getString('summary')}"`)
+        .setTimestamp()
+        .setFooter({ text: "If anything is wrong, contact @winterscode" });
+
+    await interaction.client.channels.cache.get(config.reqsChannel).send({ content: `||${departmentPing}<@${interaction.user.id}>||`, embeds: [embed] });
+
+    const embed2 = new EmbedBuilder()
+        .setColor(0xddddff)
+        .setTitle('Help Request Sent')
+        .setDescription(`You have requested help from the ${departmentPing} department. Please wait until a member can assist you.`)
+        .setTimestamp()
+        .setFooter({ text: "If anything is wrong, contact @winterscode" });
+
+    await interaction.reply({ embeds: [embed2], ephemeral: true });
 }
