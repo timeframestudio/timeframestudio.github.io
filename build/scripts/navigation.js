cdb1ad19a633ba4be73e89ddf2d93269cca082fc
timeframestudio <timeframestudiochannel@gmail.com>
2024-06-04 20:47:24 -0700

    Delete build directory

--- build/scripts/navigation.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('scroll', () => {
-    const navBar = document.querySelector('.navigation-bar');
-
-    if (window.scrollY > 0) {
-        navBar.classList.add('navigation-bar-shadowed');
-    } else {
-        navBar.classList.remove('navigation-bar-shadowed');
-    }
-});
--- build/scripts/not-found.js
+++
@@ -1,7 +0,0 @@
-import { openSearchPopup } from "./search.js";
-
-for (const element of document.querySelectorAll('.not-found-search')) {
-    element.addEventListener('click', event => {
-        openSearchPopup();
-    });
-}
