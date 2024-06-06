b7301632cd8f4d65aecc56f84ca5eb7a1c459bf2
Fuzzy Wuzzy <74162951+codeNinja-1@users.noreply.github.com>
2024-06-05 20:02:51 -0700

    Add marcoms page

--- content/departments/marketing-communications/index.ts
+++ content/departments/marketing-communications/index.ts
@@ -4,12 +4,25 @@ import { HeaderSection } from "../../../src/server/pages/sections/header-section
 import { PaddedSection } from "../../../src/server/pages/sections/padded-section.js";
 import { HeadingComponent } from "../../../src/server/pages/components/text/heading-component.js";
 import { MarkdownLoader } from "../../../src/server/pages/components/text/markdown-loader.js";
+import { VideoComponent } from "../../../src/server/pages/components/media/video-component.js";
+import { GalleryComponent } from "../../../src/server/pages/components/media/gallery-component.js";
+import { MarginComponent } from "../../../src/server/pages/components/layout/margin-component.js";
 
 class WebsiteDepartmentPage extends GeneratedPage {
     async generateWebpage(): Promise<string> {
         this.add(new PaddedSection(
-            ...MarkdownLoader.load(this.getResources().getContent('Overview')
-        )));
+            new HeadingComponent("Marketing & Communications Department"),
+            ...MarkdownLoader.load(this.getResources().getContent('Department Statement')),
+            new HeadingComponent("Logo Showcase"),
+            ...MarkdownLoader.load(this.getResources().getContent('Showcase Description')),
+            new MarginComponent(),
+            new VideoComponent(this.getResources().getContent('Showcase Video')),
+            new HeadingComponent("Flyers"),
+            ...MarkdownLoader.load(this.getResources().getContent('Flyer Description')),
+            new MarginComponent(),
+            new GalleryComponent([ 'flyer1.png', 'flyer2.png' ]),
+            new MarginComponent()
+        ));
 
         return await super.generateWebpage();
     }
